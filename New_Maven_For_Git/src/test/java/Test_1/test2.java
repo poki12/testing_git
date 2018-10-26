50bdc0746d74330d6decec1ffa60968da22322b0
paky <paky@paky-HP>
2018-10-26 16:24:30 +0200

    Test2

---
+++ New_Maven_For_Git/src/test/java/Test_1/test2.java
@@ -0,0 +1,17 @@
+package Test_1;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test2 {
+@Test
+
+public void abc(){
+	
+	Assert.assertTrue(true);
+}
+}
