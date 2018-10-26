deeebe01bb5cdcaf602ebb4bcbde5c2095465639
paky <paky@paky-HP>
2018-10-26 16:33:01 +0200

    inittttt

--- New_Maven_For_Git/src/test/java/Test_1/test2.java
+++ New_Maven_For_Git/src/test/java/Test_1/test2.java
@@ -2,10 +2,10 @@ package Test_1;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertTrue;
+
 
 import org.testng.Assert;
-import org.testng.annotations.Test;
+
 
 public class test2 {
 @Test
--- New_Maven_For_Git/src/test/java/Test_1/test_001.java
+++ New_Maven_For_Git/src/test/java/Test_1/test_001.java
@@ -14,60 +14,60 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class test_001 {
-	WebDriver driver=null;
-	
-  
-  @BeforeTest
-  public void initialize() throws Exception {
-	  
-	 
-		System.setProperty("webdriver.chrome.driver","C:\\Automation_Env\\drivers\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://accounts.google.com/signin");
-		
-  }
-  
-  @Test
-  public void GoogleLogin() throws Exception{
-
-  
-  assertEquals("Sign in", driver.findElement(By.id("signIn")).getAttribute("value"));
-  
-  
-//Maximize the window
-  driver.manage().window().maximize();
-
-  //Clear autofilled and enter username, click next
-  WebElement i= driver.findElement(By.id("identifierId"));
-  i.clear();
-  //driver.findElement(By.id("Email")).clear();
-
-  WebElement x= driver.findElement(By.id("identifierId"));
-  x.sendKeys("p.noureldeen@gmail.com");
-
-  WebElement y= driver.findElement(By.xpath(".//*[@id='identifierNext']/content/span"));
-  y.click();
-  Thread.sleep(2000);
-
-  //Clear pass and enter password, click submit
-  //*[@id='password']/div[1]/div/div[1]/input
-  driver.findElement(By.id("password")).clear();
-  driver.findElement(By.id("password")).sendKeys("++++++");
-  //driver.findElement(By.id("PersistentCookie")).click();
-  driver.findElement(By.id("passwordNext")).click();
-  Thread.sleep(2000);
-
-  //verify login is done
-  System.out.println(driver.getTitle());
-  Thread.sleep(2000);
-}
-
- 
- 
-  @AfterTest
-  public void Exit(){
-      driver.close();
+//	WebDriver driver=null;
+//	
+//  
+//  @BeforeTest
+//  public void initialize() throws Exception {
+//	  
+//	 
+//		System.setProperty("webdriver.chrome.driver","C:\\Automation_Env\\drivers\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.get("https://accounts.google.com/signin");
+//		
+//  }
+//  
+//  @Test
+//  public void GoogleLogin() throws Exception{
+//
+//  
+//  assertEquals("Sign in", driver.findElement(By.id("signIn")).getAttribute("value"));
+//  
+//  
+////Maximize the window
+//  driver.manage().window().maximize();
+//
+//  //Clear autofilled and enter username, click next
+//  WebElement i= driver.findElement(By.id("identifierId"));
+//  i.clear();
+//  //driver.findElement(By.id("Email")).clear();
+//
+//  WebElement x= driver.findElement(By.id("identifierId"));
+//  x.sendKeys("p.noureldeen@gmail.com");
+//
+//  WebElement y= driver.findElement(By.xpath(".//*[@id='identifierNext']/content/span"));
+//  y.click();
+//  Thread.sleep(2000);
+//
+//  //Clear pass and enter password, click submit
+//  //*[@id='password']/div[1]/div/div[1]/input
+//  driver.findElement(By.id("password")).clear();
+//  driver.findElement(By.id("password")).sendKeys("++++++");
+//  //driver.findElement(By.id("PersistentCookie")).click();
+//  driver.findElement(By.id("passwordNext")).click();
+//  Thread.sleep(2000);
+//
+//  //verify login is done
+//  System.out.println(driver.getTitle());
+//  Thread.sleep(2000);
+//}
+//
+// 
+// 
+//  @AfterTest
+//  public void Exit(){
+//      driver.close();
     }
 
 
-}
+//}
